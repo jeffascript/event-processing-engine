d09cd953667b7e03c4d9d3b86a4966bb1c60130a
jeffascript <ofoaro.jeff@gmail.com>
2022-04-14 20:43:56 +0200

    define event schema

---
+++ index.ts
@@ -0,0 +1,27 @@
+// An Event processing Engine using generics and mapped types
+
+/*
+  Result:
+  [
+    {
+      eventName: 'login',
+      data: { user: 'tom', name: 'tomas', hasSession: true }
+    },
+    { eventName: 'logout', data: { user: 'tom' } }
+  ]
+
+
+  */
+
+// define the class structure for the event processor
+
+//   class EventProcessor {
+//     handleEvent(eventName: ..., data: ...): void {
+//     }
+
+//     addHandler(handler: ...) {
+//     }
+
+//     getProcessedEvents(): ...[] {
+//     }
+//   }
