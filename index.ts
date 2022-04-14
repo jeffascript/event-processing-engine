70bdceeffc8abad01f086af463925a6cbcc109a7
jeffascript <ofoaro.jeff@gmail.com>
2022-04-14 21:09:12 +0200

    add handler and define dynamic types

--- index.ts
+++ index.ts
@@ -1,7 +1,57 @@
 // An Event processing Engine using generics and mapped types
 
-/*
+// define the class structure for the event processor
+
+type MapReturnType<T> = (data: T) => T;
+
+type FilterReturnType<T> = (data: T) => boolean;
+
+//generic handler type
+type Handler<T> = {
+  [Property in keyof T as `map${Capitalize<string & Property>}`]?: MapReturnType<T[Property]>;
+} & {
+  [Property in keyof T as `filter${Capitalize<string & Property>}`]?: FilterReturnType<T[Property]>;
+};
+
+class EventProcessor<T extends Record<string, unknown>> {
+  private handlers: Handler<T>[] = [];
+
+  addHandler(handler: Handler<T>) {
+    this.handlers.push(handler);
+  }
+}
+
+/* 
+Sample handler: 1. 
+EventProcessorClass.addHandler({
+    filterLogin: ({ user }) => Boolean(user),
+    mapLogin: (data) => ({
+      ...data,
+      hasSession: Boolean(data.user && data.name),
+    }),
+  })
+
   Result:
+  [
+    {
+      eventName: 'addListing',
+      data: { user: 'tom', name: 'tomas', hasAccess: true }
+    },
+    { eventName: 'removeFromListing', data: { user: 'tom' } }
+  ]
+
+
+ Sample handler: 2.
+
+  EventProcessorClass.addHandler({
+    filterLogin: ({ user }) => Boolean(user),
+    mapLogin: (data) => ({
+      ...data,
+      hasSession: Boolean(data.user && data.name),
+    }),
+  });
+
+ Result:
   [
     {
       eventName: 'login',
@@ -12,16 +62,3 @@
 
 
   */
-
-// define the class structure for the event processor
-
-//   class EventProcessor {
-//     handleEvent(eventName: ..., data: ...): void {
-//     }
-
-//     addHandler(handler: ...) {
-//     }
-
-//     getProcessedEvents(): ...[] {
-//     }
-//   }
