0dcac0a39860faf28b173e12fc2becff1188ba95
jeffascript <ofoaro.jeff@gmail.com>
2022-04-15 02:24:52 +0200

    complete the processing engine with generics

--- index.ts
+++ index.ts
@@ -13,52 +13,83 @@ type Handler<T> = {
   [Property in keyof T as `filter${Capitalize<string & Property>}`]?: FilterReturnType<T[Property]>;
 };
 
-class EventProcessor<T extends Record<string, unknown>> {
-  private handlers: Handler<T>[] = [];
+type ProcessEvent<T> = {
+  eventName: keyof T;
+  eventData: T[keyof T];
+};
+
+//shorturl.at/rABIS
+class EventProcessor<T> {
+  private handlers: Handler<T>[] = []; // [...handlers, {filterListing: () => boolean | undefined, mapListing: () => unknown}]
+  private processed: ProcessEvent<T>[] = [];
+
+  handleEvent<K extends keyof T>(eventName: K, eventData: T[K]): void {
+    let allowEvent = true;
+
+    //capitalise func
+    const capitalize = (s: string) => `${s.charAt(0).toUpperCase()}${s.slice(1)}`;
+
+    for (const handler of this.handlers) {
+      const filterFunc = handler[`filter${capitalize(eventName as string)}` as keyof Handler<T>] as unknown as ((value: T[K]) => boolean) | undefined;
+      if (filterFunc && !filterFunc(eventData)) {
+        allowEvent = false;
+        break;
+      }
+    }
+
+    if (allowEvent) {
+      let mappedEventData = { ...eventData };
+
+      //  let mappedEvents =   Object.assign({}, eventData)
+      for (const handler of this.handlers) {
+        const mapFunc = handler[`map${capitalize(<string>eventName)}` as keyof Handler<T>] as unknown as MapReturnType<T[K]> | undefined;
+        //as unknown as ((value: T[K]) => T[K]) | undefined;
+        if (mapFunc) {
+          mappedEventData = mapFunc(mappedEventData) as T[K];
+        }
+      }
+      this.processed.push({ eventName, eventData: mappedEventData });
+    }
+  }
 
   addHandler(handler: Handler<T>) {
     this.handlers.push(handler);
   }
+
+  getProcessedEvents(): ProcessEvent<T>[] {
+    return this.processed;
+  }
 }
 
-/* 
-Sample handler: 1. 
-EventProcessorClass.addHandler({
-    filterLogin: ({ user }) => Boolean(user),
-    mapLogin: (data) => ({
-      ...data,
-      hasSession: Boolean(data.user && data.name),
-    }),
-  })
+interface EventMap {
+  addLogin: { username?: string; name?: string; hasAccess?: boolean };
+  removeLogin: { username?: string };
+}
 
-  Result:
-  [
-    {
-      eventName: 'addListing',
-      data: { user: 'tom', name: 'tomas', hasAccess: true }
-    },
-    { eventName: 'removeFromListing', data: { user: 'tom' } }
-  ]
+class UserEventProcessor extends EventProcessor<EventMap> {}
 
+const userEventProcessor = new UserEventProcessor();
 
- Sample handler: 2.
+userEventProcessor.addHandler({
+  filterAddLogin: ({ username }) => Boolean(username), //data.user
+  mapAddLogin: (data) => ({ ...data, hasAccess: Boolean(data.username && data.name) }),
+});
 
-  EventProcessorClass.addHandler({
-    filterLogin: ({ user }) => Boolean(user),
-    mapLogin: (data) => ({
-      ...data,
-      hasSession: Boolean(data.user && data.name),
-    }),
-  });
+userEventProcessor.handleEvent("addLogin", { name: "Joy", username: "userIsJoy" }); //exists
+userEventProcessor.handleEvent("addLogin", { name: "tom" }); //does not exist
+userEventProcessor.handleEvent("removeLogin", { username: "joyuser" }); //should work since it is an exisiting user
+
+console.log(userEventProcessor.getProcessedEvents());
+/*
 
  Result:
-  [
-    {
-      eventName: 'login',
-      data: { user: 'tom', name: 'tomas', hasSession: true }
-    },
-    { eventName: 'logout', data: { user: 'tom' } }
-  ]
+ [
+  {
+    eventName: 'addLogin',
+    eventData: { name: 'Joy', username: 'userIsJoy', hasAccess: true }
+  },
+  { eventName: 'removeLogin', eventData: { username: 'joyuser' } }
+]
 
 
   */
